14c13709cc890fcfc0e731248f2cde8a15267ad5
hucuyuu <huangcaoyang@hotmail.com>
2020-02-09 20:23:59 +0800

    fix 了一个小问题

--- 40.c
+++ 40.c
@@ -3,31 +3,47 @@
 // 请在 s1 串 中查找 s2 串，输出其在 s1 中的位置 12 31
 // 输出是 s2 首字母在 s1 中的位置
 
-#include "stdio.h"
+#include <stdio.h>
+
+char *find(char *s1, char *s2) // 返回 s2 在 s1 中首字母的指针
+{
+	int i = 0, j = 0;
+
+	while (*(s1+i) != '\0')         // 遍历 s1
+	{
+		if (*(s1+i) == s2[j]) { // 比较 s+i 与 s2[0] 是否相同，如果相同使 j++ ，在下一次 while 中比较 s+i 与 s2[j]
+			j++;
+		}else {
+			j = 0;                  // 如果 s+i 与 s2[j] 不同，使 j=0 ，重新开始比较 s+i 与 s2[0]
+		}
+		if ((j+1) == sizeof(s2)) {      // s2 存在于 s1 条件： s 中所有字符得到比较
+			return (s1+i-j+1);      //返回 s2 首字母在 s1 中的位置
+		}
+		i++;                            // 遍历 s1
+	}
+
+	return (NULL);                  // 没找到，返回一个空指针
+}
+
 
 int main()
 {
-	char *s = NULL;
-	int i = 0, j = 0, cot = 0; // cot 是 s2 在 s1 中出现次数
+	int cot = 0; // cot 是 s2 在 s1 中出现次数
 	char s1[] = " This is a sample program and sample data.";
 	char s2[] = "sample";
+	char *p = NULL;
 
-	s = &s1[0];
+	p = find(s1, s2);
 
-	while (*(s+i) != '\0')                          // 遍历 s1
+	while (p)
 	{
-		if (*(s+i) == s2[j]) {                  // 比较 s+i 与 s2[0] 是否相同，如果相同使 j++ ，在下一次 while 中比较 s+i 与 s2[j]
-			j++;
-		}else {                                 // 如果 s+i 与 s2[j] 不同，使 j=0 ，重新开始比较 s+i 与 s2[0]
-			if (j+1 == sizeof(s2)) {        // s2 存在于 s1 条件： s 中所有字符得到比较
-				cot++;
-				printf("%d\n", i-j+1);  //输出 s2 首字母在 s1 中的位置
-			}
-			j = 0;
-		}
-		i++;                                    // 遍历 s1
+		cot++;
+		printf("%d\n", p-s1+1); // 打印 s2 首字母在 s1 中的位置
+		p = p+sizeof(s2);
+		p = find(p, s2);        // 从新位置开始寻找 s2
 	}
 
+	printf("%d\n", cot);
 	if (cot == 0) {
 		printf("没有s2\n");
 	}
