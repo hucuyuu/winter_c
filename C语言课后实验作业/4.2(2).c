1dc9fd09e76c3612085182bac97a256cd39a7b9f
hucuyuu <huangcaoyang@hotmail.com>
2020-02-25 16:23:13 +0800

    add new

--- C语言课后实验作业/4.2(2).c
+++ C语言课后实验作业/4.2(2).c
@@ -1,22 +1,23 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-	int x = 0, y = 0;
+	double x = 0, y = 0;
 
 	printf("Input x:");
-	scanf("%d", &x);
-	if (x == 0) {
+	scanf("%lf", &x);
+	if (fabs(x) < 1e-6) {
 		y = 1;
-	}else if ((x <= 5) && (x >= -5)) {
+	}else if ((x < 5.000001) && (x > -5.000001)) {
 		y = x-1;
-	}else if ((x <= 10) && (x > 5)) {
+	}else if ((x < 10.000001) && (x > 5.000000)) {
 		y = x+5;
 	}else {
 		y = 100;
 	}
 
-	printf("y:%d\n", y);
+	printf("y:%lf\n", y);
 
 	return (0);
 }
--- C课堂上机作业/24.c
+++ C课堂上机作业/24.c
@@ -8,17 +8,18 @@ int main()
 	double a = 0.0, b = 0.0, c = 0.0;
 	double delta = 0.0, x1 = 0.0, x2 = 0.0;
 
+	printf("Input a b c:");
+	scanf("%lf %lf %lf", &a, &b, &c);
 	delta = b*b - 4*a*c;
-
 	if (delta < 0) {
-		printf("wu jie\n");
+		printf("x1=%lf+i%lf,x1=%lf-i%lf \n", -b/(2.0*a), sqrt(-delta)/(2.0*a), -b/(2.0*a), sqrt(-delta)/(2.0*a));
 	}else if (fabs(delta) < 1e-6) {
 		x1 = -b/(2.0*a);
-		printf("x1=x2=%f\n", x1);
+		printf("x1=x2=%lf \n", x1);
 	}else{
 		x1 = (-b+sqrt(delta))/(2.0*a);
 		x2 = (-b-sqrt(delta))/(2.0*a);
-		printf("x1=%lf,x2=%lf\n", x1, x2);
+		printf("x1=%lf,x2=%lf \n", x1, x2);
 	}
 
 	return (0);
