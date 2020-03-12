e6660306037f481421f2a51d313b8de9f1346458
hucuyuu <huangcaoyang@hotmail.com>
2020-03-12 17:20:49 +0800

    add 3/12/2020

--- C语言课后实验作业/4.3(6).c
+++ C语言课后实验作业/4.3(6).c
@@ -1,32 +1,47 @@
 // 编写一个程序，用泰勒级数sin(x) ，计算sin(x)的值。
 // 要求最后一项的绝对值小于10-5，并统计出此时累计了多少项。
+//
+// 改造实验4.3第(6)小题：设计函数mysin，用泰勒级数计算sin(x)的值，
+// 要求最后一项的绝对值小于10-5。另外要求设计一个求正整数n!的函数fact，
+// 供mysin函数调用。在main函数中输入x，在main函数中输出数学函数sin(x)的值和泰勒级数mysin(x)的值。
+// 请认真测试程序的正确性。将源代码以文本方式提交，不要直接提交文件。
 
 #include <stdio.h>
 #include <math.h>
 
-int main()
+int cot = 0;
+
+double mysin(double num)
 {
 	int i = 1;
 	int fenmu = 1;
-	int cot = 0;
-	double x = 0;
+
 	double tmp = 0;
 	double sum = 0;
 
-	printf("Input x:");
-	scanf("%lf", &x);
 	do
 	{
-		tmp = pow(x, i)*pow(-1, i/2)/fenmu;
+		tmp = pow(num, i)*pow(-1, i/2)/fenmu;
 		sum = sum+tmp;
 		fenmu = fenmu*(i+1)*(i+2);
 		i = i+2;
 		cot++;
-		printf("%d\n", cot);
+		printf("%d  ", cot);
+		printf("%lf  ", tmp);
+		printf("%lf\n", sum);
 	} while (fabs(tmp) > 1e-5);
 
-	printf("%lf\n", tmp);
-	printf("sin(%lf):%lf\ncot:%d\n", x, sum, cot);
+	return (sum);
+}
 
+
+int main()
+{
+	double x = 0;
+
+	printf("Input x:");
+	scanf("%lf", &x);
+	printf("sin(%.5lf)=%.5lf\n", x, mysin(x));
+	printf("cot:%d\n", cot);
 	return (0);
 }
